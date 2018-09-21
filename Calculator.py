d72eb8e62eeb9fca9eb1fbdf8a4e3fb2f9fc2628
Sub-Zero-Eddy <43326108+Sub-Zero-Eddy@users.noreply.github.com>
2018-09-21 13:57:12 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,41 @@
-first_number =  int(input("Put a number here: "))
-operator = input("Put a math operator: ")
-second_number = int(input("Put a second number: "))
+# Assignment 1---------------------------------------------
+def calculator():
+    first_number = int(input("Enter a number: "))
+    operator = input("Enter an operator: ")
+    ops = ["+","-","*","/"]
+    if operator in ops:
+        second_number = int(input("Enter another number: "))
+    else:
+        print("Sorry," + "'" + operator + "'"+ " is not valid.")
+        quit_prompt = input("Press Q to quit OR enter to try again: ")
+        q_button = ["Q","q"]
+        if quit_prompt in q_button:
+            quit()
+        
+        print(calculator())
 
-def add(number1,number2):
-    print(number1 + number2)
+    def sub(number1,number2):
+        print(number1 + number2)
 
-def sub(number1,number2):
-    print(number1 - number2)
-
-def multi(number1,number2):
-    print(number1 * number2)
+    if operator == "+":
+        sub(first_number,second_number)
     
-def div(number1,number2):
-    print(number1 / number2)
+    def sub(number1,number2):
+        print(number1 - number2)
 
+    if operator == "-":
+        sub(first_number,second_number)
 
-if operator == "+":
-    add(first_number,second_number)
+    def multi(number1,number2):
+        print(number1 * number2)
 
-if operator == "-":
-    sub(first_number,second_number)
+    if operator == "*":
+         multi(first_number,second_number)
 
-if operator == "*":
-    multi(first_number,second_number)
+    def div(number1,number2):
+        print(number1 / number2)
 
-if operator == "/":
-    div(first_number,second_number)
+    if operator == "/":
+        div(first_number,second_number)
+
+calculator() 
